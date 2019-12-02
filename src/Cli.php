d3d06520c2e8f2257ab0427c1329c020fb09ed37
Viacheslav Sapronov <iamsvv@gmail.com>
2019-12-02 15:10:02 +0200

    Add third-party libraries

---
+++ src/Cli.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace BrainGames\Cli;
+
+use function \cli\line;
+use function \cli\prompt;
+
+function run()
+{
+    line('Welcome to the Brain Game!');
+    line();
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+}
